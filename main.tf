c86ff1967013d1288fe028d5b42b4472ad8339dd
amin pluralsight <verdeairo703@gmail.com>
2024-06-05 17:38:03 +0800

    adding infrastructure

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = ">=1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.43.0"
+    }
+  }
+
+  cloud {
+    organization = "ps-amin"
+
+    workspaces {
+      name = "TerraformCI"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "1-6d2401c5-playground-sandbox"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "stgrcaiml001test2"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
